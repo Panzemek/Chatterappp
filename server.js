097c6f58c4286d415a6365530991da2d94c6a2d9
Hunter Wiegand <hunter_wiegand@yahoo.com>
2019-05-06 04:03:32 -0700

    Prep for merge

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
 // Initialize Express 
 const app = express();
 
@@ -9,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 //Parse request body as a JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Initilize Passport
+app.use(passport.initialize());
 
 // Connect to the mongoDb
 mongoose.connect("mongodb://localhost/chatterappdb", { useNewUrlParser: true });
