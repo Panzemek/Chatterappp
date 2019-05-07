b4a759213baba135566393ef953b78f12650d058
Sammy Tang <samtang226@gmail.com>
2019-05-07 15:29:33 -0700

    fixed chat screens

--- chatterapp/screens/ChatroomScreen.js
+++ chatterapp/screens/ChatroomScreen.js
@@ -1,83 +1,145 @@
-import React from 'react';
-import { View, TextInput, Button, StyleSheet, Text, KeyboardAvoidingView } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat"
+import React from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  AsyncStorage
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import SocketIOClient from "socket.io-client";
 
+const USER_ID = "@userId";
 
-export default class ChatroomScreen extends React.Component {
-    state = {
-        messages: [],
-        message: "",
-        room: ""
+export default class MessageScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      room: "",
+      userId: null
+    };
+
+    this.determineUser = this.determineUser.bind(this);
+    this.onReceivedMessage = this.onReceivedMessage.bind(this);
+    this.onSend = this.onSend.bind(this);
+    this._storeMessages = this._storeMessages.bind(this);
+
+    this.socket = SocketIOClient("http://localhost:3000");
+    this.socket.on("message", this.onReceivedMessage);
+    this.determineUser();
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    return { 
+        title: navigation.getParam("pageToLoad", "Seattle"),
+        headerRight: (
+            <Button
+            onPress={() => navigation.navigate("App")}
+            title="Go Back"
+            />
+          )
     }
+  }
 
-    static navigationOptions = ({ navigation }) => {
-        return { 
-            title: navigation.getParam("pageToLoad", "Seattle"),
-            headerRight: (
-                <Button
-                onPress={() => navigation.navigate("App")}
-                title="Go Back"
-                />
-              )
+  /**
+   * When a user joins the chatroom, check if they are an existing user.
+   * If they aren't, then ask the server for a userId.
+   * Set the userId to the component's state.
+   */
+  determineUser() {
+    AsyncStorage.getItem(USER_ID)
+      .then(userId => {
+        // If there isn't a stored userId, then fetch one from the server.
+        // Todo: modify for our server structure
+        if (!userId) {
+          this.socket.emit("join", null);
+          this.socket.on("join", userId => {
+            AsyncStorage.setItem(USER_ID, userId);
+            this.setState({ userId });
+          });
+        } else {
+          this.socket.emit("userJoined", userId);
+          this.setState({ userId });
         }
-    }
+      })
+      .catch(e => alert(e));
+  }
+
+  componentDidMount() {
+    let newRoom = this.props.navigation.getParam("pageToLoad", "Seattle");
+    this.setState({ room: newRoom }, () => {});
+  }
 
 
-    componentDidMount() {
-        let newRoom = this.props.navigation.getParam("pageToLoad", "Seattle")
-        this.setState({ room: newRoom })
-    }
 
-    componentWillMount() {
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello Developer',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
-                },
-            ],
-        })
-    }
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          }
+        }
+      ]
+    });
+  }
 
-    onSend(messages = []) {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }))
-    }
+  // Event listeners
 
-    goBack = () => this.props.navigation.navigate("App");
+  onReceivedMessage(messages) {
+    this._storeMessages(messages);
+  }
 
-    render() {
-        return (
-            <KeyboardAvoidingView behavior={'padding'} style={{ flex: 1 }} keyboardVerticalOffset={100} enabled>
-                <GiftedChat
-                    messages={this.state.messages}
-                    onSend={messages => this.onSend(messages)}
-                    user={{
-                        _id: 1,
-                    }}
-                />
-            </KeyboardAvoidingView>
-        )
-    }
+  onSend(messages = []) {
+    this.socket.emit("message", messages[0]);
+    this._storeMessages(messages);
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView
+        behavior={"padding"}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={100}
+        enabled
+      >
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={this.onSend}
+          user={{_id: 1}}
+        />
+      </KeyboardAvoidingView>
+    );
+  }
+
+  // Helper functions
+  _storeMessages(messages) {
+    this.setState(previousState => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages)
+      };
+    });
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 20
-    },
+  container: {
+    padding: 20
+  },
 
-    input: {
-        height: 40,
-        backgroundColor: "rgba(000,000,000,0.2)",
-        marginBottom: 20,
-        color: "#000",
-        paddingHorizontal: 10
-    }
+  input: {
+    height: 40,
+    backgroundColor: "rgba(000,000,000,0.2)",
+    marginBottom: 20,
+    color: "#000",
+    paddingHorizontal: 10
+  }
 });
